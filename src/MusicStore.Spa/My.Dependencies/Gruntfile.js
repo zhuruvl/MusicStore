ee849dc322d90338f9ea28991b5688baf9580b2e
DamianEdwards <damian@damianedwards.com>
2014-11-19 12:31:29 -0800

    MusicStore.App: Moved grunt/bower/npm stuff to My.Dependencies folder

--- src/MusicStore.Spa/My.Dependencies/Gruntfile.js
+++ src/MusicStore.Spa/My.Dependencies/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         bower: {
 	        install: {
 			    options: {
-			        targetDir: "wwwroot/lib",
+			        targetDir: "../wwwroot/lib",
 			        layout: "byComponent",
 			        cleanTargetDir: true
 			    }
