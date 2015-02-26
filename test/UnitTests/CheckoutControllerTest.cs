d76a3048c7e174bbda0d5c49be2e6ce77dd894b6
ianhong <ianhong@outlook.com>
2015-02-25 18:00:19 -0800

    MusicStore Unit Tests (1) - MVC, EF, Cache, RequestAborted, User

---
+++ test/UnitTests/CheckoutControllerTest.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using Microsoft.AspNet.Http.Core;
+using Microsoft.AspNet.Http.Core.Collections;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using Xunit;
+using MusicStore.Controllers;
+using MusicStore.Models;
+
+namespace UnitTests
+{
+    public class CheckoutControllerTest
+    {
+        IServiceProvider _serviceProvider;
+
+        public CheckoutControllerTest()
+        {
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+                      .AddInMemoryStore()
+                      .AddDbContext<MusicStoreContext>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void AddressAndPaymente_ReturnsDefaultView()
+        {
+            // Arrange
+            var controller = new CheckoutController();
+
+            // Act
+            var result = controller.AddressAndPayment();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+        }
+
+        [Fact]
+        public void AddressAndPayment_ReturnsOrderIfInvalidPromoCode()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Form =
+                new FormCollection(new Dictionary<string, string[]>());
+
+            var controller = new CheckoutController()
+            {
+                ActionContext = new ActionContext(
+                    context,
+                    new RouteData(),
+                    new ActionDescriptor()),
+            };
+
+            var order = new Order();
+
+            // Act
+            var result = controller.AddressAndPayment(order, new CancellationToken(false)).Result;
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.NotNull(viewResult.ViewData);
+            Assert.Same(order, viewResult.ViewData.Model);
+        }
+
+        [Fact]
+        public void AddressAndPayment_ReturnsOrderIfRequestCanceled()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Form =
+                new FormCollection(new Dictionary<string, string[]>());
+
+            var controller = new CheckoutController()
+            {
+                ActionContext = new ActionContext(
+                    context,
+                    new RouteData(),
+                    new ActionDescriptor()),
+            };
+
+            var order = new Order();
+
+            // Act
+            var result = controller.AddressAndPayment(order, new CancellationToken(true)).Result;
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.NotNull(viewResult.ViewData);
+            Assert.Same(order, viewResult.ViewData.Model);
+        }
+
+        [Fact]
+        public void Complete_ReturnsOrderIdIfValid()
+        {
+            // Arrange
+            var orderId = 100;
+            var userName = "TestUserA";
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, userName) };
+
+            var httpContext = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(claims)),
+            };
+
+            var dbContext =
+                _serviceProvider.GetRequiredService<MusicStoreContext>();
+            dbContext.Add(new Order()
+            {
+                OrderId = orderId,
+                Username = userName
+            });
+            dbContext.SaveChanges();
+
+            var controller = new CheckoutController()
+            {
+                ActionContext = new ActionContext(
+                    httpContext,
+                    new RouteData(),
+                    new ActionDescriptor()),
+                DbContext = dbContext,
+            };
+
+            // Act
+            var result = controller.Complete(orderId).Result;
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.NotNull(viewResult.ViewData);
+            Assert.Equal(orderId, viewResult.ViewData.Model);
+        }
+
+        [Fact]
+        public void Complete_RetrunsErrorIfInvalidOrder()
+        {
+            // Arrange
+            var dbContext =
+                _serviceProvider.GetRequiredService<MusicStoreContext>();
+
+            var controller = new CheckoutController()
+            {
+                ActionContext = new ActionContext(
+                    new DefaultHttpContext(),
+                    new RouteData(),
+                    new ActionDescriptor()),
+                DbContext = dbContext,
+            };
+
+            // Act
+            var result = controller.Complete(100).Result;
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.Equal("Error", viewResult.ViewName);
+        }
+    }
+}
---
+++ test/UnitTests/HomeControllerTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.Cache.Memory;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using Xunit;
+using MusicStore.Controllers;
+using MusicStore.Models;
+using System.Linq;
+
+namespace UnitTests
+{
+    public class HomeControllerTest
+    {
+        IServiceProvider _serviceProvider;
+
+        public HomeControllerTest()
+        {
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+                      .AddInMemoryStore()
+                      .AddDbContext<MusicStoreContext>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void Error_ReturnsErrorView()
+        {
+            // Arrange
+            var controller = new HomeController();
+            var errorView = "~/Views/Shared/Error.cshtml";
+
+            // Act
+            var result = controller.Error();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.Equal(errorView, viewResult.ViewName);
+        }
+
+        [Fact]
+        public void Index_GetsSixTopAlbums()
+        {
+            // Arrange
+            var controller = new HomeController()
+            {
+                DbContext = _serviceProvider.GetRequiredService<MusicStoreContext>(),
+                Cache = _serviceProvider.GetRequiredService<IMemoryCache>(),
+            };
+
+            PopulateData(controller.DbContext);
+
+            // Action
+            var result = controller.Index().Result;
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotNull(viewResult.ViewData.Model);
+
+            var albums = Assert.IsType<List<Album>>(viewResult.ViewData.Model);
+            Assert.Equal(6, albums.Count);
+        }
+
+        [Fact]
+        public void StatusCodePage_ReturnsStatusCodePage()
+        {
+            // Arrange
+            var controller = new HomeController();
+            var statusCodeView = "~/Views/Shared/StatusCodePage.cshtml";
+
+            // Action
+            var result = controller.StatusCodePage();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.Equal(statusCodeView, viewResult.ViewName);
+        }
+
+        private void PopulateData(DbContext context)
+        {
+            var albums = TestAlbumDataProvider.GetAlbums();
+
+            foreach (var album in albums)
+            {
+                context.Add(album);
+            }
+
+            context.SaveChanges();
+        }
+
+        private class TestAlbumDataProvider
+        {
+            public static Album[] GetAlbums()
+            {
+                var generes = Enumerable.Range(1, 10).Select(n =>
+                    new Genre()
+                    {
+                        GenreId = n,
+                        Name = "Genre Name " + n,
+                    }).ToArray();
+
+                var artists = Enumerable.Range(1, 10).Select(n =>
+                    new Artist()
+                    {
+                        ArtistId = n + 1,
+                        Name = "Artist Name " + n,
+                    }).ToArray();
+
+                var albums = Enumerable.Range(1, 10).Select(n =>
+                    new Album()
+                    {
+                        Artist = artists[n - 1],
+                        ArtistId = n,
+                        Genre = generes[n - 1],
+                        GenreId = n,
+                    }).ToArray();
+
+                return albums;
+            }
+        }
+    }
+}
